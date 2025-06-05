8bf8d587db83dba08b9991c42b4ad5d9a0e8c6e4
max <max@bvm.network>
2025-06-05 09:57:59 +0700

    Update allowMint.sh to include additional contract addresses and modify image data URL in OnchainArtData.sol for improved NFT presentation.

--- allowMint.sh
+++ allowMint.sh
@@ -12,8 +12,16 @@
 # yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x81A509A8069cC54B5d81fAf04d76C3a744e0F532"
 # yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc"
 
-yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x5e58bde648c28e2d7234702fa3ba38585cc629c9"
-yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x212aab4bfdec109b1e3357b29b007f8e4d9df3f5"
-yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0xb44893517ae7fe7e3f77bcd39ddcc1f47b9900c9"
-yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x78C9FFF450a3504c30314240916e04BA63B5617F"
-yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x40ABA054dfE7B5AA59094B437De3dF7AA9025D46"
+# yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x5e58bde648c28e2d7234702fa3ba38585cc629c9"
+# yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x212aab4bfdec109b1e3357b29b007f8e4d9df3f5"
+# yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0xb44893517ae7fe7e3f77bcd39ddcc1f47b9900c9"
+# yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x78C9FFF450a3504c30314240916e04BA63B5617F"
+# yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x40ABA054dfE7B5AA59094B437De3dF7AA9025D46"
+
+yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x6Fe4A03d014aE65f93543A1D4cF5217e0679c7c5"
+yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x37141e8a75D0a978aa95D95892643e506b942bf7"
+yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0xA2A8c9a4FC66ac83a430c027ebD156717342D923"
+yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0x5B1BBe37b70ca8159cc32A65E227347644c86640"
+yarn ts:run scripts/nfts/cryptoai/4.allowAdmin.ts "0xD1F7f358C32Ca657B947C86856813Ecd4DfF4f8b"
+
+
